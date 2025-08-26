000905cc83257129673187c1fc8f1b50a4f44279
Maryna Kulikova <plohoeimja@gmail.com>
2025-08-26 23:52:30 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/*
 const MORSE_TABLE = {
   '.-': 'a',
   '-...': 'b',
@@ -37,8 +36,24 @@ const MORSE_TABLE = {
   '----.': '9',
   '-----': '0',
 };
-*/
 
-module.exports = function decode(/* expr */) {
-  throw new Error('Not implemented');
+module.exports = function decode(expr) {
+  let result = '';
+  for (let i = 0; i < expr.length; i += 10) {
+    const codeBlock = expr.substring(i, i + 10);
+    if (codeBlock === '**********') {
+      result += ' ';
+    } else if (codeBlock) {
+      const cleanCodeBlock = codeBlock.replace(/^0+/, '');
+      let morzeLetter = '';
+
+      for (let j = 0; j < cleanCodeBlock.length; j += 2) {
+        const pairSymb = cleanCodeBlock.substring(j, j + 2);
+        if (pairSymb === '10') morzeLetter += '.';
+        if (pairSymb === '11') morzeLetter += '-';
+      }
+      result += MORSE_TABLE[morzeLetter] || '';
+    }
+  }
+  return result;
 };
